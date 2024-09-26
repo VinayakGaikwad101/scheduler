a6dbb06aff81cdc8a98885a4e38e92944f5c594e
Rohan Nimkar <rnimkar2005@gmail.com>
2024-09-26 10:28:33 +0530

    Fixed footer component

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,15 +1,16 @@
-import React from "react";
+// import React from "react";
 import "./Footer.css"; // Make sure to import the CSS file
 
 const Footer = () => {
   return (
-    <footer className="footer text-black py-4 w-full fixed bottom-0 left-0 bg-white">
+    <footer className="footer text-black py-4 w-full h-1/10 fixed bottom-0 left-0 bg-white">
       <div className="container mx-auto px-4">
-        <div className="flex flex-col items-center justify-center space-y-2">
-          <p className="text-3xl">Made By:</p>
-          <p className="text-2xl">Vinayak Vishwanath Gaikwad (2023BCS022)</p>
-          <p className="text-2xl">Vedant Milind Khete (2023BCS024)</p>
-          <p className="text-2xl">Rohan Vinod Nimkar (2023BCS025)</p>
+        <div className="flex flex-col items-center justify-center text-white">
+          <p className="text-2xl">Developed By:</p>
+          <p className="text-xl">Vinayak Vishwanath Gaikwad (2023BCS022), Vedant Milind Khete (2023BCS024), Rohan Vinod Nimkar (2023BCS025)</p>
+        </div>
+        <div className="flex justify-end w-full">
+          <p className="text-white">2024 © All rights reserved</p>
         </div>
       </div>
     </footer>
